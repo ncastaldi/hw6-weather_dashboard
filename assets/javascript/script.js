78e3123a2fddc25d4fee5a5ad977e18507dac344
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-19 12:00:44 -0500

    create variables to store retrived data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,22 +13,41 @@ $(document).ready(function () {
 
     // Delcare JavaScript Variables
     queryCurrent = "https://api.openweathermap.org/data/2.5/weather?";
-    queryForecast = "";
+    queryUV = "http://api.openweathermap.org/data/2.5/uvi?";
+    queryForecast = "http://api.openweathermap.org/data/2.5/forecast?";
     myKey = "appid=3ba28d112b1ad87abd972d7f6388e493&q=";
 
     // Declare Functions
     function retrieveWeatherData(event) {
         event.preventDefault();
 
-        var capturedInput = cityInputEl[0].value;
+        var currentCity = cityInputEl[0].value;
 
         /* Make ajax call for "Current Weather" */
         $.ajax({
-            url: queryCurrent + myKey + capturedInput,
+            url: queryCurrent + myKey + currentCity + "units=imperial",
             method: "GET"
         }).then(function (response) {
             console.log(response);
+
+            /* Store retrieved data */
+            var currentDate = ""; //ToDo: Add moment.js to capture and display date
+            var currentTemp = response.main.temp;
+            var currentHumidity = response.main.humidity;
+            var currentWind = response.wind.speed;
+            var currLat = response.coord.lat;
+            var currLon = response.coord.lon;
+            var currUV = retrieveUVIndex(currLat, currLon);
+
         })
+    }
+
+    function retrieveUVIndex(lat, lon) {
+        $.ajax({
+            url: queryUV + myKey
+        })
+
+        return uvIndex;
     }
 
     // Make Function Calls
