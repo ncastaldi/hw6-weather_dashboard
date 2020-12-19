89cb708302d71d3dd49f2d86096d49de5e5abfaa
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-19 10:52:41 -0500

    simplified fiveDayForecast div and created DOM variables in JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,13 +3,16 @@ $(document).ready(function () {
     // Declare DOM Variables
     var storedCitiesEl = $("#storedCities");
     var currentCityEl = $("#currentCity");
+    var currentTempEl = $("#currentTemp");
+    var currentHumidityEl = $("#currentHumidity");
+    var currentWind = $("#currentWind");
+    var currentUVEl = $("currentUV");
 
     // Delcare JavaScript Variables
 
     // Declare Functions
 
     // Make Function Calls
-    console.log(currentCityEl);
 
     // Register Event Listeners
 
