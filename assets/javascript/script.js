b439554b8a884bd48c0b9cb8120a599657327af1
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-19 11:04:34 -0500

    add IDs to input and search button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
 
     // Declare DOM Variables
+    var cityInputEl = $("#cityInput");
+    var searchButton = $("#searchButton");
     var storedCitiesEl = $("#storedCities");
     var currentCityEl = $("#currentCity");
     var currentTempEl = $("#currentTemp");
     var currentHumidityEl = $("#currentHumidity");
     var currentWind = $("#currentWind");
     var currentUVEl = $("currentUV");
+    var fiveDayForecastEl = $("fiveDayForecast");
 
     // Delcare JavaScript Variables
+    myKey = ""; //ToDo: include openweather api key
+    queryURL = ""; //ToDo: include basic query url
 
     // Declare Functions
 
     // Make Function Calls
 
     // Register Event Listeners
-
+    $()
 })
