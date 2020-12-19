41c546efedf6d2d429604a22a44142dc0cd57be7
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-19 10:46:04 -0500

    simplified currentWeather div and created DOM variables in JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
 
     // Declare DOM Variables
+    var storedCitiesEl = $("#storedCities");
+    var currentCityEl = $("#currentCity");
 
     // Delcare JavaScript Variables
 
     // Declare Functions
 
     // Make Function Calls
+    console.log(currentCityEl);
 
     // Register Event Listeners
 
